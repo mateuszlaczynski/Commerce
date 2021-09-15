791ef8fc1cb46027ef012bcbb8e92a067d9a21cf
Mateusz Laczynski <mateusz.laczynski@wp.pl>
2021-09-15 10:20:23 +0200

    Added unique=True value

--- Products/models.py
+++ Products/models.py
@@ -3,8 +3,8 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=250)
-    slug = models.SlugField(blank=True, null=True)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.name
@@ -17,12 +17,12 @@ class Category(models.Model):
         return f'/{self.slug}/'
 
 class Product (models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     description = models.TextField(max_length=5000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
     image = models.ImageField(upload_to='uploads/')
 
     def __str__(self):
